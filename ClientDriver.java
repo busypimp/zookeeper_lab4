221e9d9d5d70ffd1a4a519fa490a1e55af76f491
Rachit Tyagi <rachit1503@gmail.com>
2013-04-09 19:19:57 -0400

    finished coding the file server, now working on the client driver

--- ClientDriver.java
+++ ClientDriver.java
@@ -1,12 +1,136 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 public class ClientDriver {
+
+	private static String path = "/JobTracker";
+	private static String USAGE_MESSAGE = "";
+
+	private static String hostName;
+	private static String zooKeeperServerHost;
+	private boolean redirect = false;
+	private ZkConnector connector;
+	private Watcher watcher;
+	private static Socket sock = null;
+	private static ObjectOutputStream out = null;
+	private static ObjectInputStream in = null;
+	private static String userInput;
+
+	public ClientDriver(String host) {
+		// TODO Auto-generated constructor stub
+		connector = new ZkConnector();
+		connector.connect(host);
+		watcher = getNewWatcher();
+
+	}
+
+	private Watcher getNewWatcher() {
+		// TODO Auto-generated method stub
+		return new Watcher() {
+
+			@Override
+			public void process(WatchedEvent event) {
+				// TODO Auto-generated method stub
+				handleEvent(event);
+
+			}
+
+		};
+	}
+
+	private void handleEvent(WatchedEvent event) {
+		String path = event.getPath();
+		EventType type = event.getType();
+		try {
+
+			if (path.equalsIgnoreCase(path)) {
+				if (type == EventType.NodeDeleted) {
+					System.out.println("Primary Job Tracker down");
+					Thread.sleep(5000);
+					getJobTrackerIP();
+				} else if (type == EventType.NodeCreated) {
+					System.out.println("Primary Job Tracker connected to ZooKeeper");
+					Thread.sleep(5000);
+					getJobTrackerIP();
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		if (args.length != 1) {
+			System.out.println(USAGE_MESSAGE);
+			return;
+		} else {
+			zooKeeperServerHost = args[0];
+		}
 
+		try {
+			ClientDriver cd = new ClientDriver(zooKeeperServerHost);
+			Thread.sleep(5000);
+			cd.getJobTrackerIP();
+			 sock = new Socket(hostName, 8000);
+			 in = new ObjectInputStream(sock.getInputStream());
+			 out = new ObjectOutputStream(sock.getOutputStream());
+			 System.out.println("Streams online!");
+			 /*Get user input*/
+			 BufferedReader readIn = new BufferedReader(new InputStreamReader(System.in));
+			 
+			 JobPacket packetToServer = new JobPacket();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private void getJobTrackerIP() {
+		// TODO Auto-generated method stub
+		ZooKeeper zookeeper = connector.getZooKeeper();
+		try {
+			Stat stat = zookeeper.exists(path, watcher);
+			if (stat == null) {
+				System.err.println("No job tracker exixts for <" + path + ">");
+				return;
+			}
+			byte[] IPaddr_array = zookeeper.getData(path, false, null);
+			if (hostName != null) {
+				redirect = true;
+			}
+			hostName = new String(IPaddr_array);
+			hostName = hostName.concat(".toronto.edu");
+			System.out.println("The IP address of the primary JobTracker is: <"
+					+ hostName + ">");
+		} catch (KeeperException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- FileServerHandlerThread.java
+++ FileServerHandlerThread.java
@@ -1,13 +1,126 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class FileServerHandlerThread extends Thread {
+	private Socket sock = null;
+	private String dictionary = null;
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
+	private WorkPacket packetFromClient;
+	private List<String> file_in;
+	private Map intToListMap;
 
 	public FileServerHandlerThread(Socket accept, String dictionaryFile) {
 		// TODO Auto-generated constructor stub
+		this.dictionary = dictionaryFile;
+		this.sock = accept;
+		this.file_in = readFile();
+		this.intToListMap = new HashMap();
 	}
 	
+	private List<String> readFile() {
+		List<String> fileLinesList = new ArrayList<String>();
+		try{
+			BufferedReader input_stream = new BufferedReader(new FileReader(this.dictionary));
+			String line ="";
+			while ((line = input_stream.readLine()) != null) {
+				fileLinesList.add(line);
+			}
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return fileLinesList;
+	}
+
 	public void run(){
+		try {
+			this.in = new ObjectInputStream(sock.getInputStream());
+			this.out = new ObjectOutputStream(sock.getOutputStream());
+			initPartitions();
+			
+			while((this.packetFromClient = (WorkPacket) in.readObject()) != null){
+				switch (packetFromClient.type){
+				case WorkPacket.PART_REQUEST:
+					processPartRequest();
+					break;
+				case WorkPacket.ACK:
+					System.out.println("Ack received");
+					break;
+				default:
+					System.err.println("ERROR: Unknown packet!!");
+					System.exit(-1);
+				}
+			}
+			out.close();
+			in.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void processPartRequest() {
+		System.out.println("Part request received!!");
+		
+		WorkPacket packetToClient = new WorkPacket();
+		packetToClient.type = WorkPacket.PART_REPLY;
+		packetToClient.worker_id = this.packetFromClient.worker_id;
+		packetToClient.part_id = this.packetFromClient.part_id;
+		int key = this.packetFromClient.part_id;
+		if(this.intToListMap.containsKey(key))	{
+			packetToClient.partition = (List<String>) this.intToListMap.get(key);
+			sendPacket(packetToClient);
+		}else{
+			System.err.println("Unknown part id <"+ key + "> requested!");
+		}
+	}
+
+	private void sendPacket(WorkPacket packetToClient) {
+		try {
+			out.writeObject(packetToClient);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void initPartitions() {
+		// TODO Auto-generated method stub
+		int fileSize = this.file_in.size();
+		int firstIndex, secondIndex;
+		List<String> partitionI;
+		
+		for(int i = 0; i < 4; i++){
+			firstIndex = i*fileSize;
+			secondIndex = ((i+1)*fileSize - 1);
+			
+			partitionI = this.file_in.subList(firstIndex, secondIndex);
+			this.intToListMap.put(i+1, partitionI);
+			
+		}
+//		List<String> partition_one = this.file_in.subList(0, fileSize - 1);
+//		List<String> partition_two = this.file_in.subList(fileSize, (2*fileSize) - 1);
+//		List<String> partition_three = this.file_in.subList((2*fileSize), (3*fileSize) - 1);
+//		List<String> partition_four = this.file_in.subList((3*fileSize), (4*fileSize) - 1);
 		
 	}
 
